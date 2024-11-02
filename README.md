762bbcba3b0da5dd5e3d6f707c749d500ae7184d
Arun Dhwaj <sbp.arun.01@gmail.com>
2024-11-02 20:25:31 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # masterclass_java17
-This master class contains in depth about the java17 and its features
+This master class contains in depth about the java17 and its features.
+Runtime version taken is java 23 intentionally.
+
+
---
+++ src/test/java/com/sbp/masterclass_java17/MasterclassJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.masterclass_java17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterclassJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
